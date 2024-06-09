7ed707b485e171fac2cf39b00040f34f4a849038
Yuehan Duan <yuehanduan@yahoo.com>
2024-06-09 13:02:04 -0700

    Refactor oos script to send emails and handling shipping case

--- src/website_oos_scraper.py
+++ src/website_oos_scraper.py
@@ -12,6 +12,34 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from webdriver_manager.chrome import ChromeDriverManager # Automatically manages the ChromeDriver binary
 import time # For adding delays
+import smtplib # For sending emails
+from email.mime.multipart import MIMEMultipart # For creating email messages
+from email.mime.text import MIMEText # For adding text to email messages
+from dotenv import load_dotenv # For loading environment variables from a .env file
+import os # For accessing environment variables
+
+load_dotenv() # Load environment variables from the .env file
+
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS") # Get the email address from the environment variables
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD") # Get the email password from the environment variables
+TO_EMAILS = os.getenv("TO_EMAILS").split(',') # Get the recipient email address from the environment variables
+
+def send_email(product_name, url):
+  msg = MIMEMultipart()
+  msg['From'] = EMAIL_ADDRESS
+  msg['To'] = ", ".join(TO_EMAILS)
+  msg['Subject'] = f"{product_name} is available!"
+
+  body = f"The product {product_name} is now available for pickup. Check it out here: {url}"
+  msg.attach(MIMEText(body, 'plain'))
+
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.starttls()
+  server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+  text = msg.as_string()
+  server.sendmail(EMAIL_ADDRESS, TO_EMAILS, text)
+  server.quit()
+  print("Email sent successfully.")
 
 def check_product_availability(url, product_name):
   # Creates a 'ChromeOptions' object to configure the browser options
@@ -27,21 +55,20 @@ def check_product_availability(url, product_name):
       # Use explicit wait to wait for the button to be present
       wait = WebDriverWait(driver, 10)
       pickup_element = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/main/div[5]/div/div/div/div/div/div/div[7]/div[2]/div/div[11]/div/div/div/div/div[1]/fieldset/button[1]/p')))
+      shipping_element = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/main/div[5]/div/div/div/div/div/div/div[7]/div[2]/div/div[11]/div/div/div/div/div[1]/fieldset/button[2]/p')))
 
       print("Pickup Element HTML:", pickup_element.get_attribute('outerHTML'))
+      print("Shipping Element HTML:", shipping_element.get_attribute('outerHTML'))
       # Check the text inside the button to see if it contains "Ready within"
       if "Ready within" in pickup_element.text:
           driver.quit()
           return True
+      if "Get it by" in shipping_element.text:
+          driver.quit()
+          return True
+
   except Exception as e:
       print(e)
-      # Extract and print relevant part of the page source
-      page_source = driver.page_source
-      start_index = page_source.find('<p')
-      end_index = page_source.find('</p>', start_index) + len('</p>')
-      relevant_snippet = page_source[start_index:end_index]
-
-      print("Relevant snippet of the page source:", relevant_snippet)
 
   driver.quit()
   return False
@@ -50,7 +77,18 @@ def check_product_availability(url, product_name):
 
 # Example usage
 if __name__ == "__main__": # Ensures that the following code block runs only if the script is executed directly, not when imported as a module.
-  url = "https://www.bestbuy.com/site/lenovo-loq-15-6-gaming-laptop-fhd-amd-ryzen-7-7435hs-with-16gb-memory-nvidia-geforce-rtx-4060-8gb-512gb-ssd-luna-grey/6578511.p?skuId=6578511&irclickid=yIfxa-xaexyKTxL0-0RvfWmtUkHVfkwNA3SQTI0&irgwc=1&ref=198&loc=PricePP%20LLC&acampID=0&mpid=56357&affgroup=%22Deals%22&intl=nosplash" # Specifies the URL to scrape (should be replaced with the actual target URL).
-  product_name = "Lenovo laptop" # Specifies the product name to check.
-  is_available = check_product_availability(url, product_name)
-  print(f"Product available: {is_available}")
+  url = "https://www.bestbuy.com/site/apple-11-inch-ipad-pro-4th-generation-m2-chip-wi-fi-128gb-silver/5498402.p?skuId=5498402" # Specifies the URL to scrape.
+  # url = "https://www.bestbuy.com/site/apple-airpods-with-charging-case-2nd-generation-white/6084400.p?skuId=6084400"
+  product_name = "Apple - 11-Inch iPad Pro (4th Generation) M2 chip Wi-Fi - 128GB - Silver" # Specifies the product name to check.
+  # product_name = "Apple - AirPods with Charging Case (2nd Generation) - White"
+
+  while True:
+    is_available = check_product_availability(url, product_name)
+    print(f"Product available: {is_available}")
+
+    if is_available:
+      send_email(product_name, url)
+      break # Exit the loop if the product is available
+
+    # Wait for 5 minutes before checking again
+    time.sleep(300)
